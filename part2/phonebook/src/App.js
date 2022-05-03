21ab0a0a1587f56789c7612bbacb18132ef64e16
Michaelangelo Valente <miky10.mv@gmail.com>
2022-05-03 10:36:12 +0200

    ex 2.8: The phonebook step3 - done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,41 +1,40 @@
 /**
- * 2.7 The Phonebook Step2.
+ * 2.8 The Phonebook Step3.
  * 
- * Prevent the user from being able
- * to add names that already exist in the phonebook.
- * 
- * Issue a warning alert command when such an action is attempted.
- * 
+ * Expand your application by allowing users 
+ * to add phone numbers to the phone book. 
+ * You will need to add a second input element to the 
+ * form (along with its own event handler).
  */
 
 import { useState } from 'react'
 import _ from 'lodash'
 
-const Name = ({ pName }) =>{
+const Information = ({ pName, pNumber }) =>{
   return(
-    <div>{pName}</div>
+    <div>{pName} {pNumber}</div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas' }, 
-    {name: 'Bart Simpsons'}
+    {name: 'Arto Hellas', number:'39-44-5323523'}, 
+    {name: 'Bart Simpsons', number:'40-55-6433523'}
   ])
 
   const [newName, setNewName] = useState('')
-  const [include, setInclude] = useState( true )
+  const [newNumber, setNewNumber] = useState('')
 
 
   const addName =(event)=> {
     event.preventDefault() // Prevents default action of "onSubmit"/submitting forms
-    const newPerson = { name: newName }
+    const newPerson = { name: newName, number: newNumber }
     
     // Is this the best way to do this???
     if( (persons.filter( person => person.name === newPerson.name )).length === 0 ){
-      console.log("unique")
       setPersons( persons.concat( newPerson ))
       setNewName('')
+      setNewNumber('')
     }else{
       alert(`${newPerson.name} is already added to the phonebook`)
     }
@@ -46,22 +45,28 @@ const App = () => {
  
   }
 
-  const handleChange =(event)=> {
+  const handleChangeName =(event)=> {
     setNewName(event.target.value)
   }
-
+  const handleChangeNumber =(event)=>{
+    setNewNumber(event.target.value)
+  }
  return(
     <div>
       
       <h2>Phonebook</h2>
-      
       <form onSubmit={addName}>
-
         <div>
           name: <input
                   value={newName}
-                  onChange={handleChange}                  
+                  onChange={handleChangeName}                  
                 />
+        </div>
+        <div>
+          number: < input 
+                    value={newNumber}
+                    onChange={handleChangeNumber}
+                  />
         </div>
         <div>
           <button type="submit">add</button>
@@ -72,7 +77,7 @@ const App = () => {
       <div>
         {
           persons.map( (person) => {
-            return <Name key={person.name} pName={person.name} />
+            return <Information key={person.name} pName={person.name} pNumber={person.number} />
           })
         }
         
