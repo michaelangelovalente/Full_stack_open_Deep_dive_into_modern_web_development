1e782e60ca1f562e3f561fe2f2f2373e7cbc5ce4
Michaelangelo Valente <miky10.mv@gmail.com>
2022-06-10 15:15:08 +0200

    ex 2.16: The phonebook step8 - done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,10 @@
 
 /**
-  2.15: Phonebook step7
+  2.16: Phonebook step8
   Let's return to our phonebook application.
 
-  Currently, the numbers that are added to the phonebook
-  are not saved to a backend server. Fix this situation.
-
+  Extract the code that handles the communication with the backend into
+  its own module by following the example shown earlier in this part of the course material.
 */
 
 import { useState, useEffect } from 'react'
